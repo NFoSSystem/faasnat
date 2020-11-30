fa008e5bf1d2619ca06ff3940221c0e6ee13d885
Manaphy91 <alebanfi1991@hotmail.it>
2020-11-30 21:59:57 +0100

    remember to use develop branch

--- faasnat.go
+++ faasnat.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"log"
 
-	"bitbucket.org/Manaphy91/faasnat/handlers"
-	"bitbucket.org/Manaphy91/faasnat/utils"
-	"bitbucket.org/Manaphy91/nflib"
+	"bitbucket.org/Manaphy91/faasnat/handlers@4c99c06a53"
+	"bitbucket.org/Manaphy91/faasnat/utils@4c99c06a53"
+	"bitbucket.org/Manaphy91/nflib@4c99c06a53"
 )
 
-// func main() {
-// 	// utils.Log.Println("Starting UDP Nat")
-// 	// stopChan := make(chan struct{})
-// 	// go handlers.StartIPInterface(1)
-// 	// handlers.StartUDPNat(1)
-// 	// <-stopChan
-// 	var m map[string]interface{}
-// 	Main(m)
-// }
+func main() {
+	// utils.Log.Println("Starting UDP Nat")
+	// stopChan := make(chan struct{})
+	// go handlers.StartIPInterface(1)
+	// handlers.StartUDPNat(1)
+	// <-stopChan
+	var m map[string]interface{}
+	Main(m)
+}
 
 func Main(obj map[string]interface{}) map[string]interface{} {
 	lIp, _ := nflib.GetLocalIpAddr()
